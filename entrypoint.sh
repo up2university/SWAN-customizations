cbee86ace887fb7372ed3915b76a81b93f2f1893
Michał Zimniewicz <michzimny@users.noreply.github.com>
2019-02-06 15:44:01 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,16 +9,6 @@ echo "  .Logo ..."
 mv /srv/jupyterhub/logo/logo_swan_cloudhisto.png /srv/jupyterhub/logo/logo_swan_cloudhisto.png.original
 cp up2u_logo.png /srv/jupyterhub/logo/logo_swan_cloudhisto.png
 
-### 2. Set Up2U Single Sign-On paramters
-# NOTE: Please select 'shibboleth' as authentication method ('AUTH_TYPE') for jupyterhub Docker image v0.6 and later
-echo "  .SSO Configuration..."
-cp sso_config/attribute-map.xml /etc/shibboleth/attribute-map.xml
-sed "s/%%%HOSTNAME%%%/${HOSTNAME}/" sso_config/shibboleth2.xml.template > sso_config/shibboleth2.xml
-cp sso_config/shibboleth2.xml /etc/shibboleth/shibboleth2.xml
-sed -i "s/%%%SHIBBOLETH_AUTHENTICATOR_CLASS%%%/ssotoldap_authenticator.ssotoldap_user_auth.SSOUserAuthenticator/" /srv/jupyterhub/jupyterhub_config.py
-###sed -i "s/%%%SHIBBOLETH_AUTHENTICATOR_CLASS%%%/ssoremoteuser_authenticator.sso_remote_user_auth.RemoteUserAuthenticator/" /srv/jupyterhub/jupyterhub_config.py
-
-
 echo "Done"
 echo ""
 
