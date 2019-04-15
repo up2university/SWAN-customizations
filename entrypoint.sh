147cdedebff4ab4f8e06b543f85f5f61172719f3
Michal Zimniewicz <mzimniew@man.poznan.pl>
2019-04-15 17:50:27 +0200

    disable SSO-related changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,11 @@ echo "Applying customizations for Up2U..."
 
 ### 2. Set Up2U Single Sign-On paramters
 # NOTE: Please select 'shibboleth' as authentication method ('AUTH_TYPE') for jupyterhub Docker image v0.6 and later
-echo "  .SSO Configuration..."
-cp sso_config/attribute-map.xml /etc/shibboleth/attribute-map.xml
-sed "s/%%%HOSTNAME%%%/${HOSTNAME}/" sso_config/shibboleth2.xml.template > sso_config/shibboleth2.xml
-cp sso_config/shibboleth2.xml /etc/shibboleth/shibboleth2.xml
-sed -i "s/%%%SHIBBOLETH_AUTHENTICATOR_CLASS%%%/ssotoldap_authenticator.ssotoldap_user_auth.SSOUserAuthenticator/" /srv/jupyterhub/jupyterhub_config.py
+#echo "  .SSO Configuration..."
+#cp sso_config/attribute-map.xml /etc/shibboleth/attribute-map.xml
+#sed "s/%%%HOSTNAME%%%/${HOSTNAME}/" sso_config/shibboleth2.xml.template > sso_config/shibboleth2.xml
+#cp sso_config/shibboleth2.xml /etc/shibboleth/shibboleth2.xml
+#sed -i "s/%%%SHIBBOLETH_AUTHENTICATOR_CLASS%%%/ssotoldap_authenticator.ssotoldap_user_auth.SSOUserAuthenticator/" /srv/jupyterhub/jupyterhub_config.py
 ###sed -i "s/%%%SHIBBOLETH_AUTHENTICATOR_CLASS%%%/ssoremoteuser_authenticator.sso_remote_user_auth.RemoteUserAuthenticator/" /srv/jupyterhub/jupyterhub_config.py
 
 ### 3. Add tracking code
